77656fcca9cb3e0b5fb1b2924ca7733ba20926f3
ali-avan <alixawan@gmail.com>
2024-12-04 20:42:05 +0500

    Updated SQL statement

--- Assignment 2/src/LoginApp.java
+++ Assignment 2/src/LoginApp.java
@@ -60,6 +60,7 @@ public class LoginApp extends JFrame {
         String userName = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String query = "SELECT name FROM User WHERE Email = ?";
+            String sql = "SELECT * FROM users WHERE age > 40";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, email);
             ResultSet rs = stmt.executeQuery();
